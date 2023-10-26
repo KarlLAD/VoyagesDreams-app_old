c368f30c47327148e1e7513be9693ec6e9df2c88
KarlLAD <k.ladour@gmail.com>
2023-10-26 12:20:25 -0400

    master

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // corriger erreur de la longueur  du nom de la base de données
+         Schema::defaultStringLength(191);
+
     }
 }
---
+++ database/migrations/2023_10_26_154428_create_caddie_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('caddie', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_ID')->constrained();
+            $table->foreignId('product_ID')->constrained();
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('caddie');
+    }
+};
